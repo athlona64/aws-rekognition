10c2de5d725c5f2854f9bc5bcb1f91d9f4335cb4
unknown <athlona64>
2019-10-06 21:49:10 +0700

    fix

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ dotenv.config();
 
 var s3 = new AWS.S3();
 var listenPort = process.env.PORT;
-// AWS.config.update({region:'us-east-2'});
 app.use(express.static('public'));
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({ extended: true , limit: '100mb', parameterLimit: 500000}));
@@ -131,41 +130,4 @@ app.post('/analysis', cpUpload2, function(req, res, next) {
 app.listen(listenPort, function () {
     console.log('Admin webserver listening on port ' + listenPort);
 });
-
-// AWS.config.getCredentials(function(err) {
-//   if (err) console.log(err.stack);
-//   // credentials not loaded
-//   else {
-//     console.log("Access key:", AWS.config.credentials.accessKeyId);
-//     console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
-//   }
-// });
-
-
-// console.log("Region: ", AWS.config.region);
-
-
-// // Create unique bucket name
-// var bucketName = 'node-sdk-sample-' + uuid.v4();
-// // Create name for uploaded object key
-// var keyName = 'hello_world.txt';
-
-// // Create a promise on S3 service object
-// var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
-
-// // Handle promise fulfilled/rejected states
-// bucketPromise.then(
-//   function(data) {
-//     // Create params for putObject call
-//     var objectParams = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-//     // Create object upload promise
-//     var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
-//     uploadPromise.then(
-//       function(data) {
-//         console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-//       });
-// }).catch(
-//   function(err) {
-//     console.error(err, err.stack);
-// });
 
